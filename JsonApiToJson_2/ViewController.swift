8b1363370260228cd5a7d4b0d769f9a8212737b8
Carlos Enrique Diaz Arandia <cdiaz@merqueo.com>
2020-09-03 14:31:45 -0500

    Added version controllers

--- JsonApiToJson_2/ViewController.swift
+++ JsonApiToJson_2/ViewController.swift
@@ -7,13 +7,15 @@
 //
 
 import Cocoa
+import Japx
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var inputTextfield: NSTextField!
+    @IBOutlet weak var outputTextField: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override var representedObject: Any? {
@@ -22,6 +24,32 @@ class ViewController: NSViewController {
         }
     }
 
-
+    @IBAction func btnTranslateAction(_ sender: Any) {
+        guard let data = inputTextfield.stringValue.data(using: .utf8) else { return }
+        outputTextField.stringValue = translateJsonApiToJson(at: data)
+    }
+    
+    @IBAction func btnCopyToClickBoard(_ sender: Any) {
+        copyToCliackBoard(text: outputTextField.stringValue)
+    }
+    
+    
+    private func translateJsonApiToJson(at jsonApi: Data) -> String {
+        do {
+            let jsonData = try Japx.Decoder.data(with: jsonApi)
+            let translate = String(decoding: jsonData, as: UTF8.self)
+            let replacement = translate.replacingOccurrences(of: "\\/", with: "/")
+            print("CEDA: translate: \(translate)")
+            return replacement
+        } catch let error {
+            return error.localizedDescription
+        }
+    }
+    
+    private func copyToCliackBoard(text: String) {
+        let pasteBoard = NSPasteboard.general
+        pasteBoard.clearContents()
+        pasteBoard.writeObjects([text as NSString])
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'JsonApiToJson_2' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for JsonApiToJson_2
+  pod 'Japx/Codable', '2.1.0'
+
+end
