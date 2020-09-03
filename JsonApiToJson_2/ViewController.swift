0630e99e98bb240bf275270140708d870da6c466
Carlos Enrique Diaz Arandia <cdiaz@merqueo.com>
2020-09-03 11:41:37 -0500

    Initial Commit

---
+++ JsonApiToJson_2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JsonApiToJson_2
+//
+//  Created by Carlos Diaz on 3/09/20.
+//  Copyright © 2020 Carlos Diaz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
